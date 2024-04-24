c6cf969b47d6940d4eea154a2dc66b5b9e0f778a
JaeSeung0529 <vww2021@naver.com>
2024-04-24 15:31:50 +0900

    feat:/user/login api 구현

---
+++ src/main/kotlin/com/daelim/springtest/main/api/model/dto/TestDto.kt
@@ -0,0 +1,33 @@
+package com.daelim.springtest.main.api.model.dto
+
+//data class TestDto(
+//    val id: String,
+//    val address: String,
+//    val email: String,
+//    val tel: String? = null,
+//    val age: Int,
+//)
+//
+//data class TestDtoRequest(
+//    val id: String,
+//)
+
+data class UserDto(
+    val email: String,
+    val fullname: String,
+    val password: String,
+
+)
+
+data class UserDtoRequest(
+    val fullname: String,
+    val email: String,
+    val password: String,
+)
+
+data class LottoDto(
+
+)
+data class LottoDtoRequest(
+
+)
